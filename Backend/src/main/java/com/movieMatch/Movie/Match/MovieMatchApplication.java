9bbc56265f378646eb2554110f30316ff76e197b
ateateeffeee <1023mb@gmail.com>
2022-11-08 15:00:24 -0500

    created spring template

---
+++ Backend/src/main/java/com/movieMatch/Movie/Match/MovieMatchApplication.java
@@ -0,0 +1,13 @@
+package com.movieMatch.Movie.Match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMatchApplication.class, args);
+	}
+
+}
